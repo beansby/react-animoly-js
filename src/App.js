5583878b84d5d298c6b2f9b9efa0ea50cc2ca888
Yebin Ha <beanswebdev@gmail.com>
2023-03-08 17:08:06 +0900

    folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 
-import './App.css';
+// import '././css/Style.scss';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Main from "./views/Main/Main";
+
 
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <BrowserRouter>
+        <Routes>
+            <Route exact path="/" element={<Main/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/views/Main/Main.js
@@ -0,0 +1,13 @@
+import {Component} from "react";
+
+function Main(){
+
+
+    return (
+        <div>
+            <h1> test </h1>
+        </div>
+    );
+}
+
+export default Main;
