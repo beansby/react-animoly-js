27ab3b2ebe08e06a42710bf0899b3b2de334d542
Yebin Ha <beanswebdev@gmail.com>
2023-04-14 17:48:16 +0900

    Card component - css 수정

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,7 @@ export default function Card(props){
     const animals = props.animals;
     
     return (
-        <div className="card-component container w-[930px] bg-main/5 mx-auto rounded-2xl">
+        <div className="card-component container w-[930px] mx-auto rounded-xl space-y-5">
             
             {animals.map((animal, idx)=>{
                 let region = (animal.noticeNo).substr(0,2);
@@ -21,23 +21,24 @@ export default function Card(props){
                 let age = (animal.age).substr(0, idxAge) + "년생";
 
                 return (
-                    <div key = {idx} className="flex my-4 bg-white w-[210px] h-[350px] inline-flex overflow-hidden rounded-2xl mx-[11px] border-sub border">
+                    <div key = {idx} className="flex mx-[11px] bg-white w-[210px] h-[350px] inline-flex overflow-hidden">
                         <div className="카드전체 ">
-                            <img src={animal.originalImage} alt={animal.desertionNo} className="이미지 w-[210px] h-[200px] object-cover"/>
+                            <img src={animal.originalImage} alt={animal.desertionNo} className="이미지 w-[210px] h-[200px] rounded-2xl border border-sub object-cover"/>
                             <div className="텍스트 w-[210px] h-[100px]">
                                 <div className="title text-main mt-2">
-                                    <span className="text-md text-accent font-semibold pl-4 pr-1">{age}</span>
+                                    <span className="text-md text-accent font-semibold pl-4 pr-1">{kind}</span>
+                                    <span className="text-midgrey"> {weight} </span> 
                                     {/* 성별 아이콘 */}
-                                    { animal.sex == 'F' && <img src="./icon-gender-female.png" className="card-icon"/>}
-                                    { animal.sex == 'M' && <img src="./icon-gender-male.png" className="card-icon"/>}
-                                    { animal.sex == 'Q' && <img src="./icon-gender-unknown.png" className="card-icon"/>}
-
-                                    {/* 상태 아이콘 */}
-                                    { animal.processState == '보호중' && <img src="./icon-state-protect.png" className="card-icon"/>}
-                                    { animal.processState == '공고중' && <img src="./icon-state-notice.png" className="card-icon"/>}
+                                    { animal.sex == 'F' && <img src="./icon-gender-female.png" className="card-icon-sex"/>}
+                                    { animal.sex == 'M' && <img src="./icon-gender-male.png" className="card-icon-sex"/>}
+                                    { animal.sex == 'Q' && <img src="./icon-gender-unknown.png" className="card-icon-sex"/>}
                                 </div>
                                 <div className="content px-4 text-sm text-main">
-                                    {kind} <span className="text-midgrey"> {weight} </span> <br/>
+                                    {age} &nbsp;
+                                    {/* 상태 아이콘 */}
+                                    { animal.processState == '보호중' && <img src="./icon-state-protect.png" className="card-icon-state"/>}
+                                    { animal.processState == '공고중' && <img src="./icon-state-notice.png" className="card-icon-state"/>}
+                                    <br/>
                                     <strong> 보호소 </strong>  <span className="text-accent">{region}</span> {animal.careNm} <br/>
                                     <strong> 특이사항 </strong> {animal.specialMark} <br/>
                                 </div>
